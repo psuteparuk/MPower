5326f32723807ff5289e008d573396600b53f0a8
Potcharapol <psuteparuk@gmail.com>
2014-02-10 23:15:15 -0800

    change database name

--- db/dbconfig.php
+++ db/dbconfig.php
@@ -2,11 +2,11 @@
 $host = 'us-cdbr-east-05.cleardb.net';
 $username = 'b73caf52d5e3ca';
 $password = '091cebe3';
-$db_name = 'mpower';
+$db_name = 'heroku_46b842e749abbfb';
 
 $link_db = mysql_connect($host, $username, $password);
 if (!$link_db) {
 	die("Could not connect: " . mysql_error());
 }
-//mysql_select_db($db_name);
+mysql_select_db($db_name);
 ?>
